f441b16a58812cf2e5048d1a89a4a4d0a0d6af90
bottlenecked <esemmano@gmail.com>
2018-05-21 16:01:48 +0300

    Solution: naive order receiving

--- Runnables/ReadOrderTask_2.cs
+++ Runnables/ReadOrderTask_2.cs
@@ -13,23 +13,24 @@ namespace RabbitmqSeminar.Runnables
         public void Run()
         {
             //STEP 0: grab the connection
-            var connection = ...
+            var connection = RabbitConnection.Instance;
 
             //STEP 0.1: open a channel in the connection
-            using (var channel = ...)
+            using (var channel = connection.CreateModel())
             {
                 //STEP 1: declare the orders_in queue using THE SAME configuration as before
                 //(We need to declare the queue in the consumer as well as the producer!)
-                var queue = ...
+                var queue = channel.QueueDeclare("orders_in", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                 //STEP 2: Create the consumer
-                var consumer = ...
+                var consumer = new EventingBasicConsumer(channel);
 
                 //STEP 3: attach the 'Received' event
-                consumer.Received += ...
+                consumer.Received += OnReceived;
 
                 //STEP 4: start consuming from the queue
-                queue.B...
+                channel.BasicConsume(queue, autoAck: false, consumerTag: string.Empty, noLocal: false, exclusive: false,
+                    arguments: null, consumer: consumer);
 
                 Console.WriteLine("PRESS ANY KEY TO EXIT");
                 Console.ReadKey();
@@ -39,11 +40,11 @@ namespace RabbitmqSeminar.Runnables
         private static void OnReceived(object consumer, BasicDeliverEventArgs args)
         {
             //STEP 3.1: Parse the message payload
-            var order = ...
+            var order = Encoding.UTF8.GetString(args.Body);
             Console.WriteLine($"Received order: {order}");
             Console.WriteLine();
             //STEP 3.2: Acknowledge the message delivery
-            ((IBasicConsumer)consumer).Model.B...
+            ((IBasicConsumer)consumer).Model.BasicAck(args.DeliveryTag, multiple: false);
         }
     }
 }
