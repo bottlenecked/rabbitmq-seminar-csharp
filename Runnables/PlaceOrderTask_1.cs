bbd5a32150d972514c8e788148a1c699f1ae4685
bottlenecked <esemmano@gmail.com>
2018-05-29 16:06:51 +0300

    Exercise: basic routing with direct exchange

--- Runnables/PlaceOrderTask_1.cs
+++ Runnables/PlaceOrderTask_1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQSeminar;
@@ -13,43 +14,45 @@ namespace RabbitmqSeminar.Runnables
         {
             WelcomeMessage();
 
-            // Add code here that will take an order and will send it to the orders_in queue
-            // The queue should be durable but not exclusive/autodeleted
-
-            //First grab the connection and open a channel
             var connection = RabbitConnection.Instance;
             using (var channel = connection.CreateModel())
             {
-                //We will now use an exchange to publish messages. When creating a queue it is automatically
-                //bound to the default direct exchange, and that is why we were under the illusion that we were publishing
-                //'directly' to a queue. What the client does under the covers is publish to the direct ("[empty name]") exchange
-                //using the queue name as the routing key.
-
-                //So now instead of declaring a queue, we will be declaring an exchange named 'orders_direct_exchange' and publish there
+                //Again, we are going to use a direct exchange, only this time we need to differentiate
+                //between food and drinks to help speed up order preparation (cooks use different workbenches from barmen)
 
                 const string exchangeName = "orders_direct_exchange";
-                channel.ExchangeDeclare(exchange: exchangeName, type: "direct", durable: true,
-                    autoDelete: false, arguments: null);
+                channel.ExchangeDeclare(...);
 
                 while (true)
                 {
                     //read the order and send it to rabbitmq
-                    var order = GetOrder();
-                    var bytes = Encoding.UTF8.GetBytes(order);
+                    var (category, item) = GetOrder();
+                    var bytes = Encoding.UTF8.GetBytes(item);
 
-                    //publish to the exchange created above using an empty string as routing key.
-                    channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty,
-                        basicProperties: channel.CreateBasicProperties(), body: bytes);
+                    //publish to the exchange created above using the item category as the routing key
+                    channel.BasicPublish(...);
+                    Console.WriteLine("Order sent!");
                 }
             }
         }
 
-        private static string GetOrder()
+        private static (string Category, string Item) GetOrder()
         {
-            Console.WriteLine();
-            Console.WriteLine("What would you like to order? Type in your preference and press ENTER:");
-            Console.WriteLine();
-            return Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine(
+                    "Specify either \"food:xxx\" OR \"drink:xxx\" to place your order. Type in your preference and press ENTER:");
+                Console.WriteLine();
+                var order = Console.ReadLine()
+                    ?.Split(':')
+                    .Select(x => x.Trim())
+                    .ToArray();
+                if (order != null && order.Length == 2)
+                {
+                    return (Category: order[0], Item: order[1]);
+                }
+                Console.Error.WriteLine("Invalid order.");
+            }
         }
 
         private static void WelcomeMessage()
--- Runnables/ReadOrderTask_2.cs
+++ Runnables/ReadOrderTask_2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -9,58 +10,48 @@ namespace RabbitmqSeminar.Runnables
 {
     internal class ReadOrderTask : IRunnable
     {
-        private readonly IEnumerable<string> _args;
+        private readonly string _category;
 
         public ReadOrderTask(IEnumerable<string> args)
         {
-            _args = args;
+            _category = args.FirstOrDefault()?.Trim() ?? string.Empty;
         }
 
-        public string Announcement => "Consume orders from the orders_in queue. PRESS CTRL+C TO EXIT";
+        public string Announcement => $"Consuming [{_category}] orders from the orders_in queue. PRESS CTRL+C TO EXIT";
 
         public void Run()
         {
-            //STEP 0: grab the connection
             var connection = RabbitConnection.Instance;
 
-            //STEP 0.1: open a channel in the connection
             using (var channel = connection.CreateModel())
             {
-                //STEP 1: declare the orders_in queue. What has now changed is that the queue is declared by the consumer only,
-                //since the publisher now publishes to the exchange 'orders_direct_exchange' and not 'directly' to our queue.
+                //FIRST DIFFERENCE: declare a new '[category]_in' queue as before
+                var queueName = $"{_category}_in";
+                var queue = channel.QueueDeclare(...);
 
-                var queue = channel.QueueDeclare("orders_in", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-                //STEP 1.1: declare the orders_direct_exchange exchange like the publisher did, since we must be sure that the exchange
-                //exists before binding our queue to it.
+                //declare the exchange as before
                 const string exchangeName = "orders_direct_exchange";
-                channel.ExchangeDeclare(exchange: exchangeName, type: "direct", durable: true,
-                    autoDelete: false, arguments: null);
+                channel.ExchangeDeclare(...);
 
-                //STEP 1.2: Now we need to bind the queue to the exchange (we create a binding)
-                channel.QueueBind(queue, exchangeName, routingKey: string.Empty, arguments: null);
+                //HERE IS THE OTHER DIFFERENCE: we need to bind the queue to the exchange using the category
+                //as the routing key
+                channel.QueueBind(queue, exchangeName, routingKey: _category, arguments: null);
 
-                //STEP 2: Create the consumer
                 var consumer = new EventingBasicConsumer(channel);
 
-                //STEP 3: attach the 'Received' event
                 consumer.Received += OnReceived;
 
-                //STEP 4: start consuming from the queue
-                channel.BasicConsume(queue, autoAck: false, consumerTag: string.Empty, noLocal: false, exclusive: false,
-                    arguments: null, consumer: consumer);
+                channel.BasicConsume(...);
 
                 Console.ReadKey();
             }
         }
 
-        private static void OnReceived(object consumer, BasicDeliverEventArgs args)
+        private void OnReceived(object consumer, BasicDeliverEventArgs args)
         {
-            //STEP 3.1: Parse the message payload
             var order = Encoding.UTF8.GetString(args.Body);
-            Console.WriteLine($"Received order: {order}");
+            Console.WriteLine($"Received [{_category}] order: {order}");
             Console.WriteLine();
-            //STEP 3.2: Acknowledge the message delivery
             ((IBasicConsumer)consumer).Model.BasicAck(args.DeliveryTag, multiple: false);
         }
     }
